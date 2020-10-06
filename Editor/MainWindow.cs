de47529c2ce33af603ad3f5e25d3a1b0208d67d5
Tyler Brody <brody.tyler.46@gmail.com>
2020-10-05 18:48:00 -0700

    finish move up

---
+++ Editor/MainWindow.cs
@@ -0,0 +1,99 @@
+using System;
+using System.IO;
+using Cairo;
+using Gtk;
+using UI = Gtk.Builder.ObjectAttribute;
+
+namespace Editor
+{
+    class MainWindow : Window
+    {
+        [UI] private Button _openfilebutton = null;
+        [UI] private Button _openfolderbutton = null;
+        [UI] private Button _SaveButton = null;
+        [UI] private Grid _maingrid = null;
+        [UI] private Notebook _maineditorbook = null;
+        private FolderExplorer _folderexplore = new FolderExplorer();
+
+
+
+        public MainWindow() : this(new Builder("MainWindow.glade")) { }
+
+        private MainWindow(Builder builder) : base(builder.GetObject("MainWindow").Handle)
+        {
+
+            base.SetDefaultSize(900,900);
+            CssProvider provider = new CssProvider();
+            provider.LoadFromPath(@"Styles/gtk-dark.gtk-3.0.Materia.css");
+
+            builder.Autoconnect(this);
+
+            Gtk.StyleContext.AddProviderForScreen(Gdk.Screen.Default, provider, 800); 
+
+            DeleteEvent += Window_DeleteEvent;
+            _openfilebutton.Clicked += Openfile_Clicked;
+            _openfolderbutton.Clicked += Openfolder_Clicked;
+            _folderexplore.openfile += OpenFolderOpenFile;
+            _maingrid.AttachNextTo(_folderexplore, _openfilebutton, PositionType.Bottom, 1, 5);
+            FileTextEditor.closefile += CloseEditor;
+        }
+
+        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Application.Quit();
+        }
+
+
+        private void Openfile_Clicked(object sender, EventArgs a)
+        {
+            Gtk.FileChooserDialog fc =
+            new Gtk.FileChooserDialog("Choose the file to open",
+                                        this,
+                                        FileChooserAction.Open,
+                                        "Cancel", ResponseType.Cancel,
+                                        "Open", ResponseType.Accept);
+
+            if (fc.Run() == (int)ResponseType.Accept)
+            {
+            FileTextEditor editor = new FileTextEditor();
+            editor.Setfile(fc.Filename);
+            _maineditorbook.AppendPage(editor,editor.tabutton);
+            _maineditorbook.ShowAll();
+            }
+            //Don't forget to call Destroy() or the FileChooserDialog window won't get closed.
+            fc.Dispose();
+        }
+
+        private void OpenFolderOpenFile(object sender, EventArgs a){
+            FileInfo info = sender as FileInfo;
+            FileTextEditor editor = new FileTextEditor();
+            editor.Setfile(info);
+            _maineditorbook.AppendPage(editor,editor.tabutton);
+            _maineditorbook.ShowAll();
+
+        }
+
+        private void Openfolder_Clicked(object sender, EventArgs a)
+        {
+            Gtk.FileChooserDialog fc =
+            new Gtk.FileChooserDialog("Choose the Directory to open",
+                                        this,
+                                        FileChooserAction.SelectFolder,
+                                        "Cancel", ResponseType.Cancel,
+                                        "Open", ResponseType.Accept);
+
+            if (fc.Run() == (int)ResponseType.Accept)
+            {
+                //System.Console.WriteLine(fc.Filename);
+                _folderexplore.SetDirectory(fc.Filename);
+            }
+            //Don't forget to call Destroy() or the FileChooserDialog window won't get closed.
+            fc.Dispose();
+        }
+
+        public void CloseEditor(object sender, EventArgs e){
+            _maineditorbook.Remove(sender as FileTextEditor);
+        }
+
+    }
+}
---
+++ Editor/Widgets/FolderExplorer.cs
@@ -0,0 +1,59 @@
+using Gtk;
+using System.IO;
+using System.Linq;
+using System;
+using UI = Gtk.Builder.ObjectAttribute;
+using System.Collections.Generic;
+
+class FolderExplorer : ScrolledWindow
+{
+
+    private Box box = new Box(Orientation.Vertical, 2);
+
+    private Label Current_Directory = new Label();
+
+    public event EventHandler openfile;
+
+
+    private DirectoryInfo directory;
+
+    public FolderExplorer() : base()
+    {
+        base.BorderWidth = 2;
+        Gdk.RGBA color = new Gdk.RGBA();
+        color.Parse("#0ff54c");
+        base.WidthRequest = 100;
+        base.HeightRequest = 400;
+        //base.OverrideBackgroundColor(StateFlags.Backdrop, color);
+    }
+
+    private void clickhandler(object sender, EventArgs e)
+    {
+        Button clicker = sender as Button;
+        openfile.Invoke(new FileInfo(clicker.ActionName), e);
+    }
+
+    public void SetDirectory(string path)
+    {
+        if (!System.IO.Path.EndsInDirectorySeparator(path))
+            path += System.IO.Path.DirectorySeparatorChar;
+        Console.WriteLine(path);
+        directory = new DirectoryInfo(path);
+        Current_Directory = new Label(directory.Name);
+        box.Add(Current_Directory);
+        FileInfo[] tempfiles = directory.GetFiles();
+
+        foreach (FileInfo file in tempfiles)
+        {
+            Button filebutton = new Button(file.FullName);
+            filebutton.Label = file.Name;
+            filebutton.ActionName = file.FullName;
+            filebutton.Clicked += clickhandler;
+            filebutton.Sensitive = true;
+            box.Add(filebutton);
+        }
+        box.ShowAll();
+        base.Add(box);
+        base.ShowAll();
+    }
+}
